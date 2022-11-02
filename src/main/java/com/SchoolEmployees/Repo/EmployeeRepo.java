3d7f61b53d49b9022fb4a6614659782cf8493d0c
SalehAhmed7 <111985249+SalehAhmed7@users.noreply.github.com>
2022-11-02 12:32:44 +0000

    added service layer

--- src/main/java/com/SchoolEmployees/Repo/EmployeeRepo.java
+++ src/main/java/com/SchoolEmployees/Repo/EmployeeRepo.java
@@ -1,5 +1,18 @@
 package com.SchoolEmployees.Repo;
 
-public interface EmployeeRepo {
+import java.util.List;
 
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SchoolEmployees.Domain.Employee;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository <Employee, Long> {
+
+	
+	}
+	
+	
+
+
--- src/main/java/com/SchoolEmployees/Services/EmployeeServices.java
+++ src/main/java/com/SchoolEmployees/Services/EmployeeServices.java
@@ -1,10 +1,62 @@
 package com.SchoolEmployees.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.SchoolEmployees.Domain.Employee;
+import com.SchoolEmployees.Repo.EmployeeRepo;
 
 @Service
 public class EmployeeServices {
+	
+	
+	@Autowired
+	private EmployeeRepo repo;
+
+	public EmployeeServices(EmployeeRepo repo) {
+		super();
+		this.repo = repo;
+	
+	}
+	
+	//get an employee
+	public Employee getEmployee(long id) {
+		return this.repo.findById(id).get();
+	}
+	
+	public Employee createEmployee(Employee employee) {
+		return this.repo.save(employee);
+	}
+	//get All Employees
+	public List<Employee> getEmployees(){
+		return this.repo.findAll();
+	}
+	//Adding a new employee
+	public Employee udpateEmployee(Employee newEmployee, long id) {
+		Employee found = repo.findById(id).get();
+		
+		found.setFirstName(newEmployee.getFirstName());
+		found.setLastName(newEmployee.getLastName());
+		found.setEmail(newEmployee.getEmail());
+		found.setPhoneNumber(newEmployee.getPhoneNumber());
+		found.setSubject(newEmployee.getSubject());
+		
+		return repo.save(found);
+	}
+	
+	public void deleteEmployee(long id) {
+		repo.deleteById(id);
+		
+	}
+}
 
 	
 	
-}
+	
+	
+	
+	
+	
+
